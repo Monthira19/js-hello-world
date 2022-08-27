67930f18053ee651bc2f90e2d83c905601854fa7
Monthira19 <poo19052545@hotmail.com>
2022-08-27 04:53:32 +0000

    สวัสดีมหาลัยพะเยา

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>1905 สวัสดีเมืองเชียงใหม่ HEllo</h1>')
+  return res.send('<h1>สวัสดีมหาลัยพะเยา</h1>')
 })
 
 const PORT = process.env.PORT || 3000
